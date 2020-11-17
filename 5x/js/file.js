caca33a8581c2e2fe394a798270506adfae44158
Star-Mix-hub <67189617+Star-Mix-hub@users.noreply.github.com>
2020-11-17 22:05:07 +0200

    Add files via upload

---
+++ 5x/js/file.js
@@ -0,0 +1,12 @@
+$(function(){
+$('.header-slider').slick({
+    arrows:false,
+    vertical:true,
+    dots:true,
+    dotsClass:"header-dots",
+    autoplay:1000
+});
+$(".menu-btn").on("click",function(){
+$(".menu-list").slideToggle();
+});
+});
