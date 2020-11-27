48760f933fa6d874f89f2ac9faa25e39adb7c139
Star-Mix-hub <67189617+Star-Mix-hub@users.noreply.github.com>
2020-11-27 18:54:51 +0200

    Add files via upload

---
+++ fitness/js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+$('.menu-mobail').on('click',function(){
+$('.list-item').addClass('active');
+});
+$('.close-btn').on('click',function(){
+$('.list-item').removeClass('active');
+});
+
+
+
+});
